35f2d8e338bee9eca65a1b2a2060ec1c0e5bea60
ChoChungEun <82curst@gmail.com>
2024-01-30 11:09:43 +0900

    Revert "commit a"
    This reverts commit 211ef41e69ce36aa54e7ddaf0ad8c44c97625fa3.

--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -33,7 +33,7 @@ function ItemList() {
 
   return (
     <div align="center">
-      <div>reset test1</div>
+      <div>reset test</div>
       <button onClick={handleColorChange} style={{ color: fontColor }}>
         Change Color
       </button>
